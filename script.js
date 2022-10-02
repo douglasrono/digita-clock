a9335facc75a650b1f2dddff1a49b941ac70fc5b
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-10-02 06:14:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 setInterval(showTime, 1000);
-		function showTime() {
+		const showTime => () {
 			let time = new Date();
 			let hour = time.getHours();
 			let min = time.getMinutes();
 			let sec = time.getSeconds();
-			am_pm = "AM";
+			let am_pm = "AM";
 
 			if (hour > 12) {
 				hour -= 12;
